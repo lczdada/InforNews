c243b021715dabc8e722e11cbe66619dee84b076
lczdada <lichenzhong20@foxmail.com>
2018-08-22 01:24:43 -0700

    添加了脚本启动，初始化了迁移器

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from datetime import timedelta
 
 from flask import Flask, session
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
 from flask_sqlalchemy import SQLAlchemy
 from redis import StrictRedis
 from flask_session import Session
@@ -25,7 +27,12 @@ db = SQLAlchemy(app)  # 创建数据库连接对象
 sr = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)  # 创建redis的连接对象
 Session(app)  # 初始化Session 存储对象,flask-session会自动讲session数据保存在指定的服务器端数据库里
 
-
+# 创建管理器
+mgr = Manager(app)
+# 初始化迁移命令
+Migrate(app, db)
+# 添加迁移命令
+mgr.add_command('mc', MigrateCommand)
 
 
 @app.route('/index')
@@ -34,4 +41,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run()
+    mgr.run()
