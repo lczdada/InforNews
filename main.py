ff1a6d32e46d86c4df57949e1e9477b38a9804a2
lczdada <lichenzhong20@foxmail.com>
2018-08-22 01:11:20 -0700

    session存储(重点)

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from datetime import timedelta
+
+from flask import Flask, session
 from flask_sqlalchemy import SQLAlchemy
 from redis import StrictRedis
+from flask_session import Session
+
 
 class Config:
     DEBUG = True  # 开启调试模式
@@ -8,11 +12,20 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False  # 是否追踪数据库变化
     REDIS_HOST = "127.0.0.1"  # redis的ip
     REDIS_PORT = 6379  # redis的端口
+    SESSION_TYPE = 'redis'  # session 存储的数据类型
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT) # 设置session存储使用的连接对象
+    SESSION_USE_SIGNER = True  # 对cookie中存储的sessionid进行加密, 需要使用秘钥
+    SECRET_KEY = "dWkolpXDJYbfZlzk4BvAJzcKaTvCKV+qI4YGRmIWsJUZ3JCC19cVO7AS6JQ+RUFq4ZJ3mfm7JzUay8R7hPs3Ng=="  # 应用秘钥
+    PERMANENT_SESSION_LIFETIME = timedelta(days=7)  # 默认会进行持久化,这里只需要设置时长即可
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)  # 创建数据库连接对象
 sr = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)  # 创建redis的连接对象
+Session(app)  # 初始化Session 存储对象,flask-session会自动讲session数据保存在指定的服务器端数据库里
+
+
 
 
 @app.route('/index')
