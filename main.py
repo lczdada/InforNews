884b81ad0492871b390c85acd60ac712ed8797a5
lczdada <lichenzhong20@foxmail.com>
2018-08-30 00:37:16 -0700

    增加了创建超级管理员功能以及404页面

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from flask import current_app
 from flask_migrate import MigrateCommand
 from flask_script import Manager
 from info import create_app
+
 
 app = create_app('dev')  # 创建应用,传入的参数决定环境的配置
 
@@ -11,6 +13,30 @@ mgr = Manager(app)
 mgr.add_command('mc', MigrateCommand)
 
 
+# 生成超级管理员
+@mgr.option('-u', dest='username')
+@mgr.option('-p', dest='password')
+def create_superuser(username, password):
+    if not all([username, password]):
+        print('账号/密码不完整')
+        return
+    from info import db
+    from info.models import User
+    user = User()
+    user.mobile = username
+    user.password = password
+    user.nick_name = username
+    user.is_admin = True
+    try:
+        db.session.add(user)
+        db.session.commit()
+    except BaseException as e:
+        current_app.logger.error(e)
+        db.session.rollback()
+        print('生成失败')
+
+    print('生成管理员成功')
+
 if __name__ == '__main__':
-    print(app.url_map)
+    # print(app.url_map)
     mgr.run()
